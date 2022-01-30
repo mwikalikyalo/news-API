77a353a5943d9cb34696556500621d637fbc52a4
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-31 02:01:26 +0300

    Add the configuration classes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, Flask
 from app import app
+
+app= Flask(__name__)
 
 # Views
 @app.route('/')
@@ -7,3 +9,6 @@ def index():
 
     '''View root page function that returns the index page and its data'''
     return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
