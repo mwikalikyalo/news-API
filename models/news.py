8e3e5282223154e0115731436f492f842e59f7e2
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-31 22:18:36 +0300

    Add the news test and class

---
+++ models/news.py
@@ -0,0 +1,12 @@
+class News:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,name,tagline,link,languages):
+        self.name =name
+        self.tagline = tagline
+        self.link= link
+        self.languages = languages
+        
+        
