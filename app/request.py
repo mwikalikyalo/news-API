139ac54a9d972b374b85ede4441f92d5ffc9cfda
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-31 22:25:34 +0300

    Add access to app configuration objects

--- app/request.py
+++ app/request.py
@@ -1,4 +1,13 @@
 from app import app
+from app import app
+import urllib.request,json
+from models import news
+from models.news_test import News
+
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
