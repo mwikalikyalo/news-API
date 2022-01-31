4642a202324beb5fc87399373031c29d028e2669
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-31 23:39:02 +0300

    Add the process results function

--- app/request.py
+++ app/request.py
@@ -1,3 +1,5 @@
+from concurrent.futures import process
+from unicodedata import category
 from app import app
 from app import app
 import urllib.request,json
@@ -11,3 +13,45 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        movie_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain headlines
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('headline')
+        tagline = news_item.get('tagline')
+        link= news_item.get('links')
+        languages= news_item.get('languages')
+
+        if link:
+            news_object = News(id,name,tagline,link,languages)
+            news_results.append(news_object)
+
+    return news_results
--- models/news.py
+++ models/news.py
@@ -3,7 +3,8 @@ class News:
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,name,tagline,link,languages):
+    def __init__(self,id,name,tagline,link,languages):
+        self.id=id
         self.name =name
         self.tagline = tagline
         self.link= link
