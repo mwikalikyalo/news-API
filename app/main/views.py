16de64c167f9291fb682de9f25f3f353eb28db3a
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-01 15:29:22 +0300

    Add the index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, Flask
-from app import main
+from main import app
 from app.main import app
+from app.request import get_news
 from main.config import DevConfig
 from models.news import News
 
@@ -13,21 +14,24 @@ def index():
     '''View root page function that returns the index page and its data'''
     return render_template('index.html')
 
-@app.route('/home')
+
+@app.route('/')
 def index():
   '''View root page function that returns the home page and its data'''
-  return render_template('home.html')
+  return render_template('index.html')
 
-@main.route('/news/<int:id>')
-def news(id):
+
+@app.route('/news/<int:id>')
+def news(id,name):
 
     '''
     View news page function that returns the news details page and its data
     '''
-    news= News(id)
+    news= get_news(id)
     name = f'{news.name}'
 
     return render_template('news.html',name= name,news = news)
+
 
 # Setting up configuration
 app.config.from_object(DevConfig)
--- models/news.py
+++ models/news.py
@@ -3,10 +3,11 @@ class News:
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,id,name,tagline,link,languages):
+    def __init__(self,id,name,tagline,about,link,languages):
         self.id=id
         self.name =name
         self.tagline = tagline
+        self.about= about
         self.link= link
         self.languages = languages
         
