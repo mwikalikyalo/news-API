bfcb996008db238c97bfa3b007888d588c7c7a71
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-02 03:48:32 +0300

    Add the create_app function

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,24 @@
 from flask import Flask
 from main.config import DevConfig
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
 
 # Initializing application
 app = Flask(__name__, instance_relative_config = True)
